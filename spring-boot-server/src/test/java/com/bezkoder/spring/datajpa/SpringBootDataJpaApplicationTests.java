9927ee6f15309c84576655d63aa8d95e16ed5d60
akulovairen <94855628+akulovairen@users.noreply.github.com>
2023-12-21 17:10:38 +0200

    Update SpringBootDataJpaApplicationTests.java

--- spring-boot-server/src/test/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplicationTests.java
+++ spring-boot-server/src/test/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplicationTests.java
@@ -3,9 +3,6 @@ package com.bezkoder.spring.datajpa;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
-
-
 @SpringBootTest
 class SpringBootDataJpaApplicationTests {
 
